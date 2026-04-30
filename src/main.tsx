4bc928e1462b71f497774caf6ae3991ab08a6539
n1s_01 <2905006@gmail.com>
2026-05-01 00:47:44 +0500

    chore(desktop): disable devtools and native interactions

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
 
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("dragstart", (event) => event.preventDefault());
+document.addEventListener("selectstart", (event) => event.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
